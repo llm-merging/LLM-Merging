f3202aa4d17794f0b16d3a77edd045d24a737e58
Derek Tam <dt.derek.tam@gmail.com>
2024-05-04 20:03:43 -0400

    modify yaml

--- llm_merging/setup.py
+++ llm_merging/setup.py
@@ -9,14 +9,13 @@ setup(
     version=1.0,
     description="starter code for llm_merging",
     install_requires=[
-        "torch",
+        "torch", "ipdb"
     ],
     packages=["llm_merging"],
     entry_points={
         "llm_merging.merging.Merges": [
             "llama_avg = llm_merging.merging.LlamaAvg:LlamaAvg",
             "flan_t5_avg = llm_merging.merging.FlanT5Avg:FlanT5Avg",
-            "mistral_avg = llm_merging.merging.MistralAvg:MistralAvg",
         ]
     },
 )
