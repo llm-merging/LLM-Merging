ddceae71612c49b9851e3413043ae8ee6528f369
Harshvardhan Mestha <71986193+Harshvardhan-Mestha@users.noreply.github.com>
2024-06-14 12:29:28 +0530

    Dataset V1 added.

--- src/process_math_datasets.py
+++ src/process_math_datasets.py
@@ -20,6 +20,24 @@ def process_gsm8k(row):
         "label_text": row["answer"]
     }
 
+def process_boolq(row):
+    return {
+        'question': row['question'],
+        'label': row['answer'],
+        'label_text': row["passage"],
+    }
+
+def process_mawps(row):
+    return {
+        'id': row['id'],
+        'question': row['question'],
+        'chain': row["chain"],
+        'result': row["result"],
+        'label': row["result_float"],
+        'label_text': row["equation"],
+        'expression': row["expression"],
+    }
+
 
 def process_svamp(row):
     return {
@@ -58,9 +76,21 @@ def main(args):
         splits = ['train', 'validation', 'test']
         process_fn = process_aqua
 
+    elif args.dataset == 'boolq':
+        raw_dataset = load_dataset('google/boolq')
+        demonstration_file = 'prompts/boolq/cot_prompt.txt'
+        splits = ['train', 'validation']
+        process_fn = process_boolq
+
+    elif args.dataset == 'mawps':
+        raw_dataset = load_dataset('MU-NLPC/Calc-mawps', "original-splits")
+        demonstration_file = 'prompts/mawps/cot_prompt.txt'
+        splits = ['train', 'validation', 'test']
+        process_fn = process_mawps
+
     # read the demonstration
-    with open(demonstration_file) as f:
-        demonstration = f.read()
+    # with open(demonstration_file) as f:
+    #     demonstration = f.read()
 
     if args.combine:
         combined = []
@@ -70,20 +100,49 @@ def main(args):
         for row in tqdm(raw_dataset[split],
                         desc=f'Processing {args.dataset} {split}...'):
             item = process_fn(row)
-            prompt = demonstration + f"\nQ: {item['question']} \nA: "
-            prompt_data.append({
+            # prompt = demonstration + f"\nQ: {item['question']} \nA: "
+            prompt = f"\nQ: {item['question']} \nA: "
+            if args.dataset == 'mawps':
+                prompt_data.append({
+                'id': item['id'],
                 'input': prompt,
                 'dataset': args.dataset,
                 'label': item['label'],
                 'label_text': item['label_text'],
-            })
-            if args.combine:
-                combined.append({
+                'expression': item['expression'],
+                'chain': item['chain'],
+                'result': item['result'],
+                
+                })
+                if args.combine:
+                    combined.append({
+                        'id': item['id'],
+                        'input': prompt,
+                        'dataset': args.dataset,
+                        'label': item['label'],
+                        'label_text': item['label_text'],
+                        'expression': item['expression'],
+                        'chain': item['chain'],
+                        'result': item['result'],
+                    })
+            else:
+                prompt_data.append({
                     'input': prompt,
                     'dataset': args.dataset,
                     'label': item['label'],
                     'label_text': item['label_text'],
                 })
+                if args.combine:
+                    combined.append({
+                        'input': prompt,
+                        'dataset': args.dataset,
+                        'label': item['label'],
+                        'label_text': item['label_text'],
+                    })
+
+
+
+
         write_json(
             f'./data/{args.dataset}/{split}_prompts.json',
             prompt_data,
@@ -99,7 +158,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process MATH datasets')
-    parser.add_argument('--dataset', choices=['gsm8k', 'svamp', 'aqua'])
+    parser.add_argument('--dataset', choices=['gsm8k', 'svamp', 'aqua','boolq','mawps'])
     parser.add_argument('--combine', action='store_true', default=False)
     args = parser.parse_args()
 
